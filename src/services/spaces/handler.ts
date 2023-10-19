952a9d39e2597007c36581cf5c7abeac438d662e
mikel-js <michaelcastro.js@gmail.com>
2023-10-19 22:20:16 +0300

    Update handler file

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -3,29 +3,38 @@ import {
   APIGatewayProxyResult,
   Context,
 } from 'aws-lambda';
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { postSpaces } from './PostSpaces';
+
+const ddbClient = new DynamoDBClient({});
 
 async function handler(
   event: APIGatewayProxyEvent,
   context: Context
 ): Promise<APIGatewayProxyResult> {
   let message: string;
-  switch (event.httpMethod) {
-    case 'GET':
-      message = 'Hello from GET!';
-      break;
-    case 'POST':
-      message = 'Hello from POST!';
-      break;
-    default:
-      message = 'Default';
-      break;
+  try {
+    switch (event.httpMethod) {
+      case 'GET':
+        message = 'Hello from GET!';
+        break;
+      case 'POST':
+        const response = postSpaces(event, ddbClient);
+        return response;
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
   }
   const response: APIGatewayProxyResult = {
     statusCode: 200,
     body: JSON.stringify(message),
   };
-
   return response;
 }
-
 export { handler };
