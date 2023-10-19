09fea379ca683b9b53502ffd09b189e800f684a9
mikel-js <michaelcastro.js@gmail.com>
2023-10-19 23:21:56 +0300

    Add environment variable

--- test/launcher.ts
+++ test/launcher.ts
@@ -1,10 +1,13 @@
 import { handler } from '../src/services/spaces/handler';
 
+process.env.AWS_REGION = 'eu-north-1';
+process.env.TABLE_NAME = 'SpaceTable-0eaafa956cca';
+
 handler(
   {
     httpMethod: 'POST',
     body: JSON.stringify({
-      location: 'Espoo',
+      location: 'Manila',
     }),
   } as any,
   {} as any
