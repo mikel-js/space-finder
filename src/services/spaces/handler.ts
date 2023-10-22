d1ead98d1981f291cc82c9d48e76b580e197720b
mikel-js <michaelcastro.js@gmail.com>
2023-10-22 23:08:15 +0300

    Add put request

--- src/services/spaces/handler.ts
+++ src/services/spaces/handler.ts
@@ -6,6 +6,7 @@ import {
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { postSpaces } from './PostSpaces';
 import { getSpaces } from './GetSpaces';
+import { updateSpace } from './UpdateSpace';
 
 const ddbClient = new DynamoDBClient({});
 
@@ -23,6 +24,10 @@ async function handler(
       case 'POST':
         const postResponse = await postSpaces(event, ddbClient);
         return postResponse;
+      case 'PUT':
+        const putResponse = await updateSpace(event, ddbClient);
+        console.log(putResponse);
+        return putResponse;
       default:
         break;
     }
--- test/launcher.ts
+++ test/launcher.ts
@@ -5,13 +5,17 @@ process.env.TABLE_NAME = 'SpaceTable-0eaafa956cca';
 
 handler(
   {
-    httpMethod: 'GET',
+    httpMethod: 'PUT',
     queryStringParameters: {
       id: '5d8b494f-137b-440b-830c-d632bcd38415',
     },
     // body: JSON.stringify({
     //   location: 'Manila',
     // }),
+    // },
+    body: JSON.stringify({
+      location: 'Dublin updated',
+    }),
   } as any,
   {} as any
 );
