0b56fcdd61b501b9edcfaf91e90fdeb64ef4ba9b
mikel-js <michaelcastro.js@gmail.com>
2023-10-26 23:57:23 +0300

    add auth test

---
+++ test/auth.test.ts
@@ -0,0 +1,9 @@
+import { AuthService } from './AuthService';
+
+async function testAuth() {
+  const service = new AuthService();
+  const loginResult = await service.login('barosanu', 'sdf45aAf574fg)');
+  console.log(loginResult);
+}
+
+testAuth();
