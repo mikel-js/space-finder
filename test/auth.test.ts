17ac485c994232230c9365377f86f65cbe4587fc
mikel-js <michaelcastro.js@gmail.com>
2023-10-27 23:22:45 +0300

    update test

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -2,7 +2,7 @@ import { AuthService } from './AuthService';
 
 async function testAuth() {
   const service = new AuthService();
-  const loginResult = await service.login('barosanu', 'sdf45aAf574fg)');
+  const loginResult = await service.login('duke', 'Thykel1217.');
   console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
 }
 
