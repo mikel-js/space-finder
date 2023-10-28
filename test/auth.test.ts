cff6f9f51de29bf472e2e296b90b95346dd686f7
mikel-js <michaelcastro.js@gmail.com>
2023-10-28 17:16:38 +0300

    update test

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -1,11 +1,22 @@
 import { AuthService } from './AuthService';
-
+import { ListBucketsCommand, S3Client } from '@aws-sdk/client-s3';
 async function testAuth() {
   const service = new AuthService();
   const loginResult = await service.login('duke', 'Thykel1217.');
   // console.log(loginResult.getSignInUserSession().getIdToken().getJwtToken());
   const credentials = await service.generateTemporaryCredentials(loginResult);
-  console.log(credentials);
+  // console.log(credentials);
+  const buckets = await listBuckets(credentials);
+  console.log(buckets);
+}
+
+async function listBuckets(credentials: any) {
+  const client = new S3Client({
+    credentials: credentials,
+  });
+  const command = new ListBucketsCommand({});
+  const result = await client.send(command);
+  return result;
 }
 
 testAuth();
